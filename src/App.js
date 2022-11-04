8dbfd108308022879f0f0a71b9adaf3807247ee5
Yesmith Tamayo <Yesmith.Y.Tamayo.-ND@disney.com>
2022-11-04 14:43:04 -0500

    react-hooks-01: add hooks on the app to get data from api

--- src/App.js
+++ src/App.js
@@ -1,44 +1,38 @@
 import React, {Component} from 'react';
+import { useState, useEffect } from 'react';
 import Motorcycles from './components/motorcycles';
 import axios from 'axios';
 
-class App extends Component {
 
-    render() {
-        return (
-            <Motorcycles motorcycles={this.state.motorcycles} />
-        )
+function App() {
+    const [motorcycles, setMotorcycles] = useState([]);
+    const [error, setError] = useState(null);
+    const options = {
+        method: 'GET',
+        url: 'https://motorcycle-specs-database.p.rapidapi.com/model/make-name/BMW',
+        headers: {
+            'X-RapidAPI-Key': '13b0f0d627msh71afbcc2d57667ep143c7ajsnb6fad3acd0cc',
+            'X-RapidAPI-Host': 'motorcycle-specs-database.p.rapidapi.com'
+        }
     }
 
-    state = {
-        motorcycles: []
-    };
-
-    componentDidMount() {
-        const options = {
-            method: 'GET',
-            url: 'https://motorcycle-specs-database.p.rapidapi.com/model/make-name/BMW',
-            headers: {
-              'X-RapidAPI-Key': '13b0f0d627msh71afbcc2d57667ep143c7ajsnb6fad3acd0cc',
-              'X-RapidAPI-Host': 'motorcycle-specs-database.p.rapidapi.com'
-            }
-        };
-        var me = this;
-
+    useEffect(() => {
         axios.request(options)
             .then(function (response) {
                 let data = response && response.data;
-                
-                if(data.length > 0){
-                    // console.log(data.length, response.data, this);
-                    me.setState({ 
-                        motorcycles: data 
-                    });
-                }
-            }).catch(function (error) {
-                console.error(error);
-            });
-    }
+
+                setMotorcycles(data);
+                setError(null);
+            }).catch(setError);
+      }, []);
+
+      if (error) return <p>An error occurred</p>
+
+    return (
+        <div className="App">
+            <Motorcycles motorcycles={motorcycles}/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/motorcycles.js
+++ src/components/motorcycles.js
@@ -6,7 +6,7 @@ const Motorcycles = ({motorcycles}) => {
         <div>
             <center><h1>Motorcycles List</h1></center>
             {motorcycles.map((motorcycle) => (
-                <div className="card">
+                <div className="card" key={motorcycle.id}>
                     <div className="card-body">
                         <h6 className="card-subtitle mb-2 text-muted">{motorcycle.id}</h6>
                         <h5 className="card-title">{motorcycle.name}</h5>
