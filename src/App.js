2ae247dccddb005fd34a6b5726e6dd95c9d9c96f
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2022-11-15 13:42:39 -0500

    add-loading-02: Basic loading state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { useState, useEffect } from 'react';
 import Motorcycles from './components/motorcycles';
 import axios from 'axios';
 
+const options = {
+  method: 'GET',
+  url: 'https://motorcycle-specs-database.p.rapidapi.com/model/make-name/BMW',
+  headers: {
+      'X-RapidAPI-Key': '13b0f0d627msh71afbcc2d57667ep143c7ajsnb6fad3acd0cc',
+      'X-RapidAPI-Host': 'motorcycle-specs-database.p.rapidapi.com'
+  }
+}
 
 function App() {
     const [motorcycles, setMotorcycles] = useState([]);
+    const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    const options = {
-        method: 'GET',
-        url: 'https://motorcycle-specs-database.p.rapidapi.com/model/make-name/BMW',
-        headers: {
-            'X-RapidAPI-Key': '13b0f0d627msh71afbcc2d57667ep143c7ajsnb6fad3acd0cc',
-            'X-RapidAPI-Host': 'motorcycle-specs-database.p.rapidapi.com'
-        }
-    }
 
     useEffect(() => {
         axios.request(options)
@@ -23,10 +24,16 @@ function App() {
 
                 setMotorcycles(data);
                 setError(null);
-            }).catch(setError);
-      }, []);
-
-      if (error) return <p>An error occurred</p>
+            })
+            .catch(setError)
+            .finally(function () {
+              setLoading(false)
+            });
+    }, []);
+    
+    if (loading) return <p>Loading...</p>
+    
+    if (error) return <p>An error occurred</p>
 
     return (
         <div className="App">
